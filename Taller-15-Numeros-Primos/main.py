2f7ace7bec4dfbd0877caa25f0b64e1d5f7a127e
VASQUEZ NAVAS CARLOS <cavasquezna@uide.edu.ec>
2026-08-20 19:46:04 -0500

    taller15-numeros-primos

--- Taller-15-Numeros-Primos/main.py
+++ Taller-15-Numeros-Primos/main.py
@@ -1,33 +1,36 @@
 import random
 
 
-# Ejercicio A
-# Generar un número aleatorio entre 1 y 100 que sea primo.
-
 def es_primo(numero):
     if numero < 2:
         return False
 
-    for i in range(2, int(numero ** 0.5) + 1):
-        if numero % i == 0:
+    for divisor in range(2, numero):
+        if numero % divisor == 0:
             return False
 
     return True
 
 
-numeros_primos = [numero for numero in range(1, 101) if es_primo(numero)]
-numero_aleatorio = random.choice(numeros_primos)
+# EJERCICIO A
+# Generar un número primo aleatorio entre 1 y 100
 
-print("Ejercicio A")
-print("Número primo aleatorio:", numero_aleatorio)
+while True:
+    numero = random.randint(1, 100)
+
+    if es_primo(numero):
+        break
+
+print("EJERCICIO A")
+print("Número primo generado:", numero)
 
 
-# Ejercicio B
-# Pedir un valor N y mostrar todos los números primos hasta N.
+# EJERCICIO B
+# Mostrar todos los números primos hasta N
 
-n = int(input("\nEjercicio B\nIngrese un valor N: "))
+n = int(input("\nEJERCICIO B\nIngrese un número N: "))
 
-print(f"Números primos hasta {n}:")
+print("Números primos hasta", n, ":")
 
 for numero in range(2, n + 1):
     if es_primo(numero):
